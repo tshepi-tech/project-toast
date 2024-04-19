2551bcf536f5c1a0f0a3738e6192fb463ee72a27
tshepiso <tshepi.tech@gmail.com>
2024-04-19 11:05:08 +0200

    toast hidden by default. pops on button

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -10,6 +10,7 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
 	const [message, setMessage] = React.useState("");
 	const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+	const [toastOn, setToastOn] = React.useState(false);
 
 	return (
 		<div className={styles.wrapper}>
@@ -17,7 +18,7 @@ function ToastPlayground() {
 				<img alt="Cute toast mascot" src="/toast.png" />
 				<h1>Toast Playground</h1>
 			</header>
-			<Toast>{message}</Toast>
+			{toastOn && <Toast>{message}</Toast>}
 			<div className={styles.controlsWrapper}>
 				<div className={styles.row}>
 					<label
@@ -63,13 +64,7 @@ function ToastPlayground() {
 				<div className={styles.row}>
 					<div className={styles.label} />
 					<div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-						<Button
-							onClick={() => {
-								window.alert(`${variant} - ${message}`);
-							}}
-						>
-							Pop Toast!
-						</Button>
+						<Button onClick={() => setToastOn(!toastOn)}>Pop Toast!</Button>
 					</div>
 				</div>
 			</div>
